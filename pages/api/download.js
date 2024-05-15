018cefd832559ef4b8f5ec499a814ba88cb1d968
adriel45dev <45adrielalves45@gmail.com>
2024-05-15 16:51:59 -0300

    🔧 DOWNLOAD FIX

--- pages/api/download.js
+++ pages/api/download.js
@@ -1,6 +1,8 @@
 import torrentStream from "torrent-stream";
 import path from "path";
 
+const MAX_STREAM_DURATION = 60 * 1000; // 1 minuto
+
 export default async (req, res) => {
   if (req.method !== "GET") {
     res.status(405).json({ message: "Method Not Allowed" });
@@ -20,26 +22,47 @@ export default async (req, res) => {
     const engine = torrentStream(torrentHash);
 
     engine.on("ready", () => {
-      const file = engine.files.find(
+      const fileIndex = engine.files.findIndex(
         (f) => f.path === decodeURIComponent(filePath)
       );
 
-      if (!file) {
+      if (fileIndex === -1) {
         res.status(404).json({ message: "File not found" });
         return;
       }
 
+      const file = engine.files[fileIndex];
       const stream = file.createReadStream();
-      stream.on("error", (err) => {
-        console.error("Error streaming file:", err);
-        res.status(500).json({ message: "Internal Server Error" });
-      });
+
+      let timeout = setTimeout(() => {
+        stream.destroy();
+        res.status(504).json({ message: "Request timed out" });
+      }, MAX_STREAM_DURATION);
 
       res.setHeader(
         "Content-Disposition",
         `attachment; filename="${path.basename(file.name)}"`
       );
-      stream.pipe(res);
+
+      stream.on("data", (chunk) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          stream.destroy();
+          res.status(504).json({ message: "Request timed out" });
+        }, MAX_STREAM_DURATION);
+        res.write(chunk);
+      });
+
+      stream.on("end", () => {
+        clearTimeout(timeout);
+        res.end();
+      });
+
+      stream.on("error", (err) => {
+        console.error("Error streaming file:", err);
+        clearTimeout(timeout);
+        res.status(500).json({ message: "Internal Server Error" });
+      });
     });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error" });
